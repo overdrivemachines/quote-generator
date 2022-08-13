9d72f20486459c6625c0bcaf9377b679b12f5ab0
get.dipen@gmail.com <get.dipen@gmail.com>
2022-08-13 11:13:21 -0700

    Fetching quotes from API

---
+++ script.js
@@ -0,0 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  console.log(quote);
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch(error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getQuotes();
